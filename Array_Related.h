c1845aeb4cf003a52a0ad825e6bf251077025dc9
kevinxrzhu(朱枭汝) <kevinxrzhu@tencent.com>
2020-07-23 17:54:52 +0800

    add apiral matrix question

--- Array_Related.h
+++ Array_Related.h
@@ -709,7 +709,7 @@ public:
         return n + 1;
     }
 
-    // #42 *********
+    // #42 ********* Trapping Rain Water
     // Use left and right pointer
     // The current capacity depends on the difference between the min value of the left/right barrier(pointer)
     // and the current barrier
@@ -770,6 +770,24 @@ public:
         return ret;
     }
 
+
+    // #59 Spiral Matrix
+    static vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> ret(n, vector<int>(n));
+        int up = 0, down = n - 1, left = 0, right = n-1, val = 1;
+
+        while(true){
+            for (int j = left; j <=right; ++j) ret[up][j] = val++;
+            if (++up > down) break;
+            for (int i = up; i <= down; ++i) ret[i][right] = val++;
+            if (--right < left) break;
+            for (int j = right; j >= left; --j) ret[down][j] = val++;
+            if (--down < up) break;
+            for (int i = down; i >= up; --i) ret[i][left] = val++;
+            if (++left > right) break;
+        }
+        return ret;
+    }
 
 };
 
