d911a121f9718a05f809290531d280e639c23d02
KevinXiaoruZhu <zhuxiaoru101@gmail.com>
2020-08-05 17:58:17 +0800

    63 64

--- Array_Related.h
+++ Array_Related.h
@@ -805,6 +805,68 @@ public:
         }
         return dp[n - 1];
     }
+
+    // #63 Unique Paths II (With Obstacle) - DP
+    static int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = (int)obstacleGrid.size(), n = (int)obstacleGrid[0].size();
+        if (m == 0 || n == 0) return 0;
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; ++i){
+            if (!obstacleGrid[i][0]) dp[i][0] = 1;
+            else break;
+        }
+        for (int i = 0; i < n; ++i){
+            if (!obstacleGrid[0][i]) dp[0][i] = 1;
+            else break;
+        }
+        if (m == 1) return dp[0][n-1];
+        if (n == 1) return dp[m-1][0];
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (obstacleGrid[i][j]) {
+                    dp[i][j] = 0; continue;
+                }
+                dp[i][j] += dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    // #64 Minimum Path Sum - DP
+    static int minPathSum(vector<vector<int>>& grid) {
+        int m = (int)grid.size(), n = (int)grid[0].size();
+        if (m == 0 || n == 0) return 0;
+        int val = 0;
+        if (m == 1){
+            for (int i = 0; i < n; ++i) val += grid[0][i];
+            return val;
+        }
+        if (n == 1){
+            for (int i = 0; i < m; ++i) val += grid[i][0];
+            return val;
+        }
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < n; ++i){
+            val += grid[0][i];
+            dp[0][i] = val;
+        }
+        val = 0;
+        for (int i = 0; i < m; ++i){
+            val += grid[i][0];
+            dp[i][0] = val;
+        }
+        for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j) {
+                int tmp;
+                dp[i-1][j] >= dp[i][j-1] ? tmp = dp[i][j-1] : tmp = dp[i-1][j];
+                dp[i][j] = grid[i][j] + tmp;
+            }
+
+        return dp[m-1][n-1];
+
+    }
+
 };
 
 #endif //ALGORITHMPRACTICE_ARRAY_RELATED_H
