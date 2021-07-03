5fbb6c15ebdd1d386b556f587e09c167dd37c689
unknown <zhuxiaoru101@gmail.com>
2021-07-02 23:57:52 -0700

    update

--- Array_Related.h
+++ Array_Related.h
@@ -1154,7 +1154,36 @@ static int subarraySumEqualsKII(vector<int> &nums, int k) {
         }
         sum2idx[prefix_sum[end + 1]] = end + 1;
     }
+    if (rst == 0x3f3f3f3f) {
+        return -1;
+    }
     return rst;
+}
+
+// #Lint1840 mid
+/**
+     * @param n: the row of the matrix
+     * @param m: the column of the matrix
+     * @param after: the matrix
+     * @return: restore the matrix
+     */
+vector<vector<int>> matrixRestoration(int n, int m, vector<vector<int>> &after) {
+    vector<vector<int>> before(n, vector<int>(m, 0));
+    before[0][0] = after[0][0];
+    for (int i = 1; i < n; ++i) {
+        before[i][0] = after[i][0] - after[i-1][0];
+    }
+    for (int j = 1; j < m; ++j) {
+        before[0][j] = after[0][j] - after[0][j-1];
+    }
+
+    for (int i = 1; i < n; ++i) {
+        for (int j = 1; j < m; ++j) {
+            before[i][j] = after[i][j] - after[i-1][j] - after[i][j-1] + after[i-1][j-1];
+        }
+    }
+
+    return before;
 }
 
 // #Lint1507 hard
